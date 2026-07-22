a67e237dd563ee95974dd9a0fda294fe7b38e062
mdrafidkhan34-source <mdrafidkhan34@gmail.com>
2026-07-23 01:32:23 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 const video = document.getElementById("video");
 
-// URL থেকে video প্যারামিটার নেওয়া
 const params = new URLSearchParams(window.location.search);
 const videoUrl = params.get("video");
 
+const player = new Plyr(video, {
+    controls: [
+        "play-large",
+        "rewind",
+        "play",
+        "fast-forward",
+        "progress",
+        "current-time",
+        "duration",
+        "mute",
+        "volume",
+        "settings",
+        "pip",
+        "fullscreen"
+    ]
+});
+
 if (videoUrl) {
     video.src = decodeURIComponent(videoUrl);
-    video.load();
-
-    // অটো প্লে করার চেষ্টা
-    video.play().catch(() => {
-        console.log("Autoplay blocked.");
-    });
-} else {
-    document.body.innerHTML = `
-        <div style="
-            color:white;
-            text-align:center;
-            font-family:Arial;
-            padding:40px;">
-            <h2>No Video Found</h2>
-            <p>Please provide a video URL.</p>
-        </div>
-    `;
+    video.play();
 }
